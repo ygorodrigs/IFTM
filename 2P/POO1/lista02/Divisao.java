a4b01a2f4d353db20ecb59595422744ab865ffa5
Ygor Rodrigues <ygorodrigs@gmail.com>
2026-09-14 19:34:46 -0300

    Refactor error handling in dividir method
    Updated error message handling in dividir method.

--- 2P/POO1/lista02/Divisao.java
+++ 2P/POO1/lista02/Divisao.java
@@ -9,7 +9,7 @@ public class Divisao {
 
     public double dividir() {
         if (b == 0) {
-            System.out.println("Erro: Divisão por zero!");
+            System.out.println("Erro: Divisão por zero!"); // a interação com o usuário é mais usual fazer no Main, separar essa parte
             return 0;
         }
         return (double) a/b;
